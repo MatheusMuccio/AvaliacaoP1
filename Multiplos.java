ec2b71929d4fbc90f5fb5eae5a962a2c7be70e90
Matheus Muccio <matheus_muccio@outlook.com>
2022-04-04 16:58:11 +0000

    Exercício-02.

--- Multiplos.java
+++ Multiplos.java
@@ -15,6 +15,13 @@ Exibirá assim: 0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90 e 9
 
 public class Multiplos {
     public static void main(String[] args) {
+
+        for (int index = 0; index < 100; index++) {
+            if (index % 6 == 0) {
+                System.out.println("Número múltiplo de 6: " + index);
+            }
+            
+        }
         
     }
     
