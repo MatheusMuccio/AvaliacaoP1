2e93e25df30d032be532d4140b17a7e7283ec02c
Matheus Muccio <matheus_muccio@outlook.com>
2022-04-04 16:49:30 +0000

    Exercício-01

---
+++ BalancoTrimestral.java
@@ -0,0 +1,19 @@
+/* Crie uma classe chamada BalancoTrimestral com um bloco main, dentro do main (o miolo do programa), declare uma variável inteira chamada
+gastosAbril e inicialize-a como o valor de um salário mínimo;
+Exiba o salário-mínimo mutiplicado por 10. 
+
+Exercício 01.
+
+*/
+
+public class BalancoTrimestral{
+    public static void main(String[] args) {
+
+        double gastosAbril = 1212.00; //Valor salário mínimo.
+
+        System.out.printf("Valor salário mínimo: %.2f R$",gastosAbril," R$");
+        System.out.printf("\nValor salário mínimo multiplicado x10: %.2f R$", (gastosAbril * 10)," R$");
+        
+    }
+
+}
